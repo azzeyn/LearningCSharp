4328b2a57e9543140a29252797621eca596af0e3
Kuba <jacob.tulacz@gmail.com>
2024-07-09 19:32:19 +0200

    add string to numbers convertion to Program.cs files in String_Chars

--- String_Chars/Program.cs
+++ String_Chars/Program.cs
@@ -10,7 +10,27 @@ namespace String_Chars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("22");
+            string textAge = "-23";
+            int age = Convert.ToInt32(textAge);
+            Console.WriteLine(age);
+
+            string textBigNumber = "-900000000";
+            long bigNumber = Convert.ToInt64(textBigNumber);
+            Console.WriteLine(bigNumber);
+
+            string textNegative = "-55.2";
+            double negative = Convert.ToDouble(textNegative);
+            Console.WriteLine(negative);
+
+            string textPrecision = "5.0000001";
+            float precision = Convert.ToSingle(textPrecision);
+            Console.WriteLine(precision);
+
+            string decimalText = "10.50";
+            decimal decimalNumber = Convert.ToDecimal(decimalText);
+            Console.WriteLine(decimalNumber);
+
+            Console.ReadLine();
         }
     }
 }
