ca0f3d2f4fc0851357125a0eb73218d6d528d326
Kuba <jacob.tulacz@gmail.com>
2024-07-09 18:21:20 +0200

    add changes to database

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -10,7 +10,10 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            
+            int x = 0,
+                y = 1,
+                z = 3;
+
             int age = -22;
             Console.WriteLine(age);
             Console.WriteLine(int.MaxValue);
@@ -20,6 +23,21 @@ namespace MyFirstProject
             Console.WriteLine(bigNumber);
             Console.WriteLine(long.MaxValue);
             Console.WriteLine(long.MinValue);
+
+            double negative = -55.2D;
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+            float precision = 5.0000001F;
+            Console.WriteLine(precision);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+
+            decimal decimalNumber = 10.50M;
+            Console.WriteLine(decimalNumber);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
 
 
             Console.ReadLine(); 
