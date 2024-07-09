5e4221f9789315ed1cb16d16be968b972eb2f14d
Kuba <jacob.tulacz@gmail.com>
2024-07-09 19:44:21 +0200

    add changes to the program.cs file in Bool project

--- Bool/Program.cs
+++ Bool/Program.cs
@@ -10,7 +10,16 @@ namespace Bool
     {
         static void Main(string[] args)
         {
+            bool value = true;
+            Console.WriteLine(value);
+            bool isMale = true;
+            Console.WriteLine(isMale);
 
+            isMale = false;
+            Console.WriteLine(isMale);
+
+
+            Console.ReadLine();
         }
     }
 }
