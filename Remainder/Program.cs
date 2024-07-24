c264051d1f17666a948ebecf79df1d739b402cfd
Kuba <jacob.tulacz@gmail.com>
2024-07-24 17:57:03 +0200

    add changes to the program.cs in remainder project

--- Remainder/Program.cs
+++ Remainder/Program.cs
@@ -10,6 +10,18 @@ namespace Remainder
     {
         static void Main(string[] args)
         {
+            int firstNum = 10;
+            int secondNum = 2;
+
+            // 10 / 3 = 3r1
+            // 3x3 = 9, 1 left over to get to 10
+            Console.WriteLine(firstNum % secondNum);
+
+            Console.WriteLine(1000 % 90);
+            Console.WriteLine(100 % 90);
+            Console.WriteLine(71 % 10);
+
+            Console.ReadLine();
         }
     }
 }
