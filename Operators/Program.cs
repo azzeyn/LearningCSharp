58d9b12f94181cea3ef15487edb35f835d0ad7c1
Kuba <jacob.tulacz@gmail.com>
2024-07-24 17:44:49 +0200

    add changes to file program.cs in operators project

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -10,6 +10,21 @@ namespace Operators
     {
         static void Main(string[] args)
         {
+            int age = 23;
+            // + - * /
+            age += 30;
+            Console.WriteLine(age);
+
+            string name = "Aba";
+            name += " is programming";
+            Console.WriteLine(name);
+
+            // Weird results cause you're trying to add unicode values of chars
+            char ch = 'a';
+            ch += 'b';
+            Console.WriteLine(ch);
+
+            Console.ReadLine();
         }
     }
 }
