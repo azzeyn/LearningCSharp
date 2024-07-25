4f139ed8137cce7aa3761215293c8882239a1f75
Kuba <jacob.tulacz@gmail.com>
2024-07-25 17:31:47 +0200

    add storing user data exercise

---
+++ StoringUserData/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoringUserData
+{
+    internal class Program
+    {
+        /*
+         * Define a variable to hold your name
+         * Define a variable to hold your phone number
+         * Define a variable to hold your age
+         * Print variables line-by-line to the screen
+         * Extra: define variables using the var keyword
+         */
+        static void Main(string[] args)
+        {
+            string name = "Jacob";
+            string phoneNumber = "050000000";
+            int age = 22;
+            Console.WriteLine(name);
+            Console.WriteLine(phoneNumber);
+            Console.WriteLine(age);
+
+            var intNumber = 5011010;
+            Console.WriteLine(intNumber);
+
+            Console.ReadLine();
+        }
+    }
+}
