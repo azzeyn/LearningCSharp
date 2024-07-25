f4fe348a7bbf1bea2ae4be436cf55fabc43d3c91
Kuba <jacob.tulacz@gmail.com>
2024-07-25 17:40:46 +0200

    add OddOrEven exercise

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOrEven
+{
+    internal class Program
+    {
+        /*
+         * Create & initaliste two ints
+         * Make a variable to work out the remainder
+         * Output remainder to the screen
+         * Change the first int variable to another number
+            * and recalculate the remainder
+            * output new remainder to the screen    
+         */
+        static void Main(string[] args)
+        {
+            int firstNumber = 10;
+            int secondNumber = 2;
+            int remainder = firstNumber % secondNumber;
+            Console.WriteLine(remainder);
+
+            firstNumber = 15;
+            remainder = firstNumber % secondNumber;
+            Console.WriteLine(remainder);
+
+            Console.ReadLine();
+        }
+    }
+}
