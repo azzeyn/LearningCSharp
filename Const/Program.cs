4ec35166fbb515ec85b5a024f7658c8fa422760d
Kuba <jacob.tulacz@gmail.com>
2024-07-25 17:21:30 +0200

    add const keyword projects

---
+++ Const/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int vat = 20;
+            const double percentVAT = vat / 100D;
+
+            int balance = 1000;
+
+            // We give it a 100D - so now it's a decimal number
+            Console.WriteLine(balance * percentVAT);
+
+            const string version = "v1.0";
+
+            Console.ReadLine();
+        }
+    }
+}
