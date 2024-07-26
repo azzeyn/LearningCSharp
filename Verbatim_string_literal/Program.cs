0c32cf19c7c0699d5b9d6e3de1c7b60a9492f513
Kuba <jacob.tulacz@gmail.com>
2024-07-26 19:32:27 +0200

    add Verbatim_string_literal project

---
+++ Verbatim_string_literal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verbatim_string_literal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Backslash is a escape character
+            string speech = "He said \"something\"" ;
+            Console.WriteLine(speech);
+            string path = "C:\\Users\\Jakub\\Desktop\\LearningCSharp\nNew line test";
+            Console.WriteLine(path);
+            // $ +
+            // $"Your name is {name}"
+            // "Your name is " + name
+
+            path = @"\Users\Jakub\Desktop\LearningCSharp" + "\nNew line test";
+            Console.WriteLine(path);
+
+            string name = @"Hello ""someone"" ";
+
+            // It's okay because it doesn't interrupt the syntax of the language. The character can just be inside the string and to the compiler it's just a char even tho they're similar
+            name = "Hello 'someone'";
+            Console.WriteLine(name);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Verbatim_string_literal/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Verbatim_string_literal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Verbatim_string_literal")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("830a2d9e-5ad4-4ffc-a4f1-0faf28b82ce8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
