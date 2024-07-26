45a79204639bcbebe7c76b0e0b92936f0b49c44d
Kuba <jacob.tulacz@gmail.com>
2024-07-26 18:32:15 +0200

    add Times_Table project

---
+++ Times_Table/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Times_Table
+{
+    internal class Program
+    {
+        /*
+         * Ask the user for a number for the table
+         * Write a for loop to print X times table
+         */
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int.TryParse(Console.ReadLine(), out int num);
+
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", i, num, num * i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
