e20f797c4fbeaeb1159a6800ecd9948695ab874d
Kuba <jacob.tulacz@gmail.com>
2024-07-26 19:54:25 +0200

    Add String_interpolation project

---
+++ String_interpolation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_interpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jacob";
+            int age = 22;
+
+            Console.WriteLine($"Your name is {name}, and your age is {age}.");
+            Console.ReadLine();
+        }
+    }
+}
