1659abaf853eabce5b67b0d88d69ee018bb603bb
Kuba <jacob.tulacz@gmail.com>
2024-07-26 16:24:31 +0200

    add Conditional_operator project

---
+++ Conditional_operator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_operator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+
+            /*
+            if (age >= 0)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            */
+
+            // condition ? true : false
+
+            Console.WriteLine(age >= 0 ? "Valid" : "Invalid");
+
+            Console.ReadLine();
+        }
+    }
+}
