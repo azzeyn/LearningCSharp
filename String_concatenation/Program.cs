9f61589d2ed256852514bead1b07b60352693bf5
Kuba <jacob.tulacz@gmail.com>
2024-07-27 17:00:33 +0200

    add String_concatenation project

---
+++ String_concatenation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_concatenation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jacob";
+            int age = 22;
+
+            string test = string.Concat("Your name is ", name, " and your age is ", age);
+            Console.WriteLine(test);
+
+
+            Console.ReadLine();
+        }
+    }
+}
