e862e33ad47bafb23130a5f0be72c8c1981c6481
Kuba <jacob.tulacz@gmail.com>
2024-07-28 13:03:30 +0200

    add String_iteration project

--- String_iteration/Program.cs
+++ String_iteration/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace String_iteration
 {
@@ -10,6 +12,28 @@ namespace String_iteration
     {
         static void Main(string[] args)
         {
+            string message = "C# is awesome";
+            // string is an array of characters
+            // Console.WriteLine(message[0]);
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine();
+            Console.WriteLine(message.Contains("C"));
+
+            bool contains = false;
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if(message[i].Equals('C'))
+                {
+                    contains = true;
+                }
+            }
+            Console.WriteLine(contains);
 
             Console.ReadLine();
         }
