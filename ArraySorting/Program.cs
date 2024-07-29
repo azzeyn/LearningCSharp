53a6dd0b8e568ab1bb5fbe5cb20d59124d8d70f9
Kuba <jacob.tulacz@gmail.com>
2024-07-29 10:31:42 +0200

    Add ArraySorting project

---
+++ ArraySorting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Compiler will count it for us and give it a length
+            int[] numbers = new int[]
+            {
+                111, 2, 33, 4, 59, 6, 7, 8
+            };
+
+            Array.Sort(numbers);
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
