19697dab0192b27ac8285d79ab7eb83e91e42849
Kuba <jacob.tulacz@gmail.com>
2024-07-30 09:14:43 +0200

    Add Lists project

---
+++ Lists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3]
+                {
+                    1, 2, 3 
+                };
+
+            List<int> listNumbers = new List<int>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter a number: ");
+                listNumbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            listNumbers.RemoveAt(0);
+
+            for (int i = 0; i < listNumbers.Count; i++)
+            {
+                Console.WriteLine(listNumbers[i]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
