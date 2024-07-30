b41f294ad5cb3049619b5b9519311e250cfce5b4
Kuba <jacob.tulacz@gmail.com>
2024-07-30 18:00:02 +0200

    Add VoidFunctions project

---
+++ VoidFunctions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+
+            CreateAndPrintArray();
+            Console.ReadLine();
+        }
+
+        static void CreateAndPrintArray()
+        {
+            int[] numbers = new int[5] {0, 1, 2, 3, 4};
+
+            foreach (var item in numbers)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to my program");
+        }
+    }
+}
