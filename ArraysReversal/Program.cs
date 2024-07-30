5241622d9b360dd31b927debb7d3d0a9dd1d207b
Kuba <jacob.tulacz@gmail.com>
2024-07-30 08:01:45 +0200

    Add ArraysReversal project

---
+++ ArraysReversal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysReversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[]
+            {
+                0, 1, 2, 3, 4 ,5 , 6
+            };
+
+            int[] sortedNumbers = new int[numbers.Length];
+
+            int x = 0;
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                sortedNumbers[x] = numbers[i];
+                x++;
+            }
+
+            foreach (var num in sortedNumbers)
+            {
+                Console.WriteLine(num);
+            }
+            /*
+
+            Array.Reverse(numbers);
+
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+            */
+
+            Console.ReadLine();
+        }
+    }
+}
