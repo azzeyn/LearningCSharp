0f683491504a07c8132fa887bad84bf361c1269a
Kuba <jacob.tulacz@gmail.com>
2024-07-31 09:56:01 +0200

    Add OptionalParameters project

---
+++ OptionalParameters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Add(5);
+            Console.WriteLine(result);
+
+            PrintName();
+
+            Console.ReadLine();
+        }
+
+        static void PrintName(string name = "Jakub")
+        {
+            Console.WriteLine($"My name is {name}");
+        }
+
+        // [Optional] - by default will give us the basic value for this number
+        static int Add(int a, [Optional] int b)
+        {
+            Console.WriteLine($"a = {a}\nb = {b}");
+            return a + b; 
+        }
+    }
+}
