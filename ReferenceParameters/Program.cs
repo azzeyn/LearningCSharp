4b569b32c66c029869864637732abe6ef627ac5d
Kuba <jacob.tulacz@gmail.com>
2024-08-01 09:43:38 +0200

    Add ReferenceParameters project

---
+++ ReferenceParameters/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int num = 10;
+            Assign(ref num, ref name);
+            Console.WriteLine(num);
+            Console.WriteLine(name);
+            */
+
+            string name = "";
+
+            Console.Write("Enter your new name: ");
+            string newName = Console.ReadLine();
+
+            if (ChangeName(ref name, newName))
+            {
+                Console.WriteLine($"You've changed your name to: {newName}");
+            }
+            else
+            {
+                Console.WriteLine("New name cannot be empty or null!");
+            }
+
+            Console.ReadLine();
+        }
+
+        static bool ChangeName(ref string name, string newName)
+        {
+            if (!string.IsNullOrEmpty(newName))
+            {
+                name = newName;
+                // return jumps to the end of the function
+                return true;
+            }
+            return false;
+        }
+
+        static void Assign(ref int num, ref string name)
+        {
+            num = 20;
+            name = "Jakub";
+        }
+    }
+}
