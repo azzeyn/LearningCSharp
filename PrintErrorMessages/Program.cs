58da1a53d96aedf4f7b5337370995954f3512159
Kuba <jacob.tulacz@gmail.com>
2024-08-02 10:19:01 +0200

    Add PrintErrorMessages project

---
+++ PrintErrorMessages/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintErrorMessages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num);
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Please only enter numbers!");
+            }
+
+            catch (OverflowException)
+            {
+                Console.WriteLine("Please enter a value under 2 billion!");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
