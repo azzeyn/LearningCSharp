956cc1582eaf16015657adfba665c977028405de
Kuba <jacob.tulacz@gmail.com>
2024-08-08 17:03:53 +0200

    Add ArrayShoppingList project

---
+++ ArrayShoppingList/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayShoppingList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = -1;
+
+            string[] arr =
+            {
+                "Milk", "Bread", "Cheese", "Soda", "Water"
+            };
+
+            Console.Write("Enter an item you want to serach in the shopping list: ");
+            string item = Console.ReadLine().ToLower();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i].ToLower().Equals(item))
+                {
+                    index = i;
+                }
+            }
+
+            Console.WriteLine(index > -1 ? $"{arr[index]} was found on index {index}\n" : "Item not found\n");
+
+            Console.WriteLine("Full Item List:\n");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {arr[i]}");
+            }
+
+            // Second approach
+
+            int position = Array.IndexOf(arr, item);
+            Console.WriteLine();
+            Console.WriteLine(position > -1 ? $"{arr[position]} was found on index {position}\n" : "Item not found\n");
+            
+            Console.WriteLine("Full Item List:\n");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {arr[i]}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
